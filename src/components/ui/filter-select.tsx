1aedc60aa2f633c11d927d6f83aad3c590d6a8cf
Alessandro Benig <alebenig4@gmail.com>
2025-09-09 00:22:28 +0800

    feat: add filter select component

---
+++ src/components/ui/filter-select.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Portal, Select, createListCollection } from "@chakra-ui/react";
+
+type FilterSelectProps = {
+	placeholder: string;
+	items: { label: string; value: string }[];
+	value: string;
+	setValue: (value: string) => void;
+};
+
+export default function FilterSelect({ placeholder, items, value, setValue }: FilterSelectProps) {
+	const collection = createListCollection({ items });
+
+	return (
+		<Select.Root
+			collection={collection}
+			width={{ base: "full", md: "200px" }}
+			value={[value]}
+			onValueChange={(e) => setValue(e.value.at(0) || "")}
+		>
+			<Select.HiddenSelect />
+			<Select.Control>
+				<Select.Trigger>
+					<Select.ValueText placeholder={placeholder} />
+				</Select.Trigger>
+				<Select.IndicatorGroup>
+					<Select.Indicator />
+				</Select.IndicatorGroup>
+			</Select.Control>
+			<Portal>
+				<Select.Positioner>
+					<Select.Content>
+						{collection.items.map((item) => (
+							<Select.Item key={item.value} item={item}>
+								{item.label}
+								<Select.ItemIndicator />
+							</Select.Item>
+						))}
+					</Select.Content>
+				</Select.Positioner>
+			</Portal>
+		</Select.Root>
+	);
+}
