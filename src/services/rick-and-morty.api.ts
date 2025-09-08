98e3e73292481c30c48e9cf59c85debb30b1a89b
Alessandro Benig <alebenig4@gmail.com>
2025-09-09 01:36:15 +0800

    feat: add signal and thrown error for failed request

--- src/services/rick-and-morty.api.ts
+++ src/services/rick-and-morty.api.ts
@@ -2,32 +2,44 @@ import { TCharacter, TCharacterQueryParams, TResponse } from "@/types";
 
 const BASE_URL = "https://rickandmortyapi.com/api";
 
-export async function getCharacters(params: TCharacterQueryParams = {}) {
+// characters
+export async function getCharacters(params: TCharacterQueryParams = {}, signal?: AbortSignal) {
 	const url = new URL(`${BASE_URL}/character`);
 	url.search = new URLSearchParams(params).toString();
 
 	const res = await fetch(url, {
 		method: "GET",
+		signal,
 	});
+
+	if (!res.ok) throw new Error("Failed to fetch characters");
 
 	const data = await res.json();
 	return data as TResponse<TCharacter>;
 }
 
-export async function getEpisodes() {
-	const res = await fetch(`${BASE_URL}/character`, {
+// episodes
+export async function getEpisodes(signal?: AbortSignal) {
+	const res = await fetch(`${BASE_URL}/episode`, {
 		method: "GET",
+		signal,
 	});
 
+	if (!res.ok) throw new Error("Failed to fetch episodes");
+
 	const data = await res.json();
-	return data as TResponse<TCharacter>;
+	return data;
 }
 
-export async function getLocations() {
+// locations
+export async function getLocations(signal?: AbortSignal) {
 	const res = await fetch(`${BASE_URL}/location`, {
 		method: "GET",
+		signal,
 	});
 
+	if (!res.ok) throw new Error("Failed to fetch locations");
+
 	const data = await res.json();
-	return data as TResponse<TCharacter>;
+	return data;
 }
