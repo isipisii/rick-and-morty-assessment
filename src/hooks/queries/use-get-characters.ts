2d90403fc83101fec292d15c9c111e50a6deaee3
Alessandro Benig <alebenig4@gmail.com>
2025-09-09 00:22:54 +0800

    feat: add query parameters to useGetCharacters function

--- src/hooks/queries/use-get-characters.ts
+++ src/hooks/queries/use-get-characters.ts
@@ -1,13 +1,14 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getCharacters } from "@/services/rick-and-morty.api";
+import { TCharacterQueryParams } from "@/types";
 
 const CHARACTERS_KEY = "CHARACTERS";
 
-export function useGetCharacters() {
+export function useGetCharacters(params: TCharacterQueryParams = {}) {
 	return useInfiniteQuery({
-		queryKey: [CHARACTERS_KEY],
+		queryKey: [CHARACTERS_KEY, ...Object.values(params)],
 		initialPageParam: "1",
-		queryFn: ({ pageParam }) => getCharacters({ page: pageParam.toString() }),
+		queryFn: ({ pageParam }) => getCharacters({ page: pageParam.toString(), ...params }),
 		getNextPageParam: (lastPage) => {
 			if (!lastPage.info.next) return undefined;
 
