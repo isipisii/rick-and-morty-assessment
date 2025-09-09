2ebb66146d860929a047e16bc533030c8fcfd68d
Alessandro Benig <alebenig4@gmail.com>
2025-09-09 10:41:52 +0800

    feat: implement character details page with loading spinner

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import CharacterDetails from "@/components/character-details";
+import { Flex, Spinner } from "@chakra-ui/react";
+import React, { Suspense } from "react";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
 	const id = (await params).id;
 
-	return <div>page</div>;
+	return (
+		<Suspense
+			fallback={
+				<Flex justify="center" align="center" h="100vh">
+					<Spinner size="xl" />
+				</Flex>
+			}
+		>
+			<CharacterDetails id={id} />
+		</Suspense>
+	);
 }
